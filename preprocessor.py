5bf43e19f84a1e692dfd59f2fc5c98f1a082f702
khushi-engg <sukanyaojha8@gmail.com>
2026-02-27 15:32:44 +0530

    Add files via upload

---
+++ preprocessor.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def preprocess():
+    df = pd.read_csv(
+        'athlete_events.csv',
+        encoding='latin1',
+        encoding_errors='replace'
+    )
+    region_df = pd.read_csv(
+        'noc_regions.csv',
+        encoding='latin1',
+        encoding_errors='replace'
+    )
+
+    df = df[df['Season'] == 'Summer']
+    df = df.merge(region_df, on='NOC', how='left')
+    df['region'].fillna('Unknown', inplace=True)
+    df = df.drop_duplicates()
+
+    df = pd.concat([df, pd.get_dummies(df['Medal'])], axis=1)
+
+    return df

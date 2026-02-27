e628d5bc8bed35cd0eb8bcc9f651dccd3cb22c39
khushi-engg <sukanyaojha8@gmail.com>
2026-02-27 16:00:51 +0530

    Add files via upload

---
+++ preprocessor.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+def preprocess():
+    # Try UTF-8 first
+    try:
+        df = pd.read_csv('athlete_events.csv', encoding='utf-8')
+    except UnicodeDecodeError:
+        df = pd.read_csv(
+            'athlete_events.csv',
+            encoding='latin1',
+            encoding_errors='ignore'
+        )
+
+    try:
+        region_df = pd.read_csv('noc_regions.csv', encoding='utf-8')
+    except UnicodeDecodeError:
+        region_df = pd.read_csv(
+            'noc_regions.csv',
+            encoding='latin1',
+            encoding_errors='ignore'
+        )
+
+    df = df[df['Season'] == 'Summer']
+    df = df.merge(region_df, on='NOC', how='left')
+    df['region'].fillna('Unknown', inplace=True)
+    df = df.drop_duplicates()
+
+    df = pd.concat([df, pd.get_dummies(df['Medal'])], axis=1)
+
+    return df
